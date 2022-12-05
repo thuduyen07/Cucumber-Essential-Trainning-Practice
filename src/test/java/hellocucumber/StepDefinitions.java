cbb34a06f97a66df602b10fbc8a1866d782857a0
thuduyen07 <thuduyen07@gmail.com>
2022-12-05 17:53:54 +0700

    add StepDefinitions for is_it_friday feature

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -18,4 +18,19 @@ public class StepDefinitions {
     public void theScenarioPasses() {
     }
 
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("today is sunday");
+    }
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("I ask whether it's Friday yet");
+    }
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("I should be told " + string);
+    }
 }
