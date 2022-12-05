d23bc96424c1aee58621c54dae5a0508b8d6af9b
thuduyen07 <thuduyen07@gmail.com>
2022-12-05 17:59:52 +0700

    add body for StepDefinitions methods

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -2,9 +2,12 @@ package hellocucumber;
 
 import io.cucumber.java.en.*;
 
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
+
+    private String today;
+    private String actualAnswer = "Nope";
 
     @Given("an example scenario")
     public void anExampleScenario() {
@@ -22,15 +25,27 @@ public class StepDefinitions {
     public void today_is_sunday() {
         // Write code here that turns the phrase above into concrete actions
         System.out.println("today is sunday");
+        today = "Sunday";
     }
+
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_friday_yet() {
         // Write code here that turns the phrase above into concrete actions
         System.out.println("I ask whether it's Friday yet");
+        if(IsItFriday.isItFriday(today))
+            actualAnswer = "Yes";
     }
+
     @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
+    public void i_should_be_told(String expectedAnswer) {
         // Write code here that turns the phrase above into concrete actions
-        System.out.println("I should be told " + string);
+        System.out.println("I should be told " + expectedAnswer);
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
+
+class IsItFriday {
+    static boolean isItFriday(String today) {
+        return today.equals("Friday");
     }
 }
